8b4b58c81e751eacf67f4e1350e13a7080087d63
Cody Abbott <cody.abbott16@gmail.com>
2020-02-11 16:55:36 +0000

    library class is passing mvp

---
+++ src/main/java/Library.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Library {
+
+    private int capacity;
+    private ArrayList<Book> books;
+
+    public Library(int capacity){
+        this.capacity = capacity;
+        this.books = new ArrayList<Book>();
+    }
+
+    public int numberOfBooks() {
+        return this.books.size();
+    }
+
+    public void addBook(Book book) {
+        if (this.books.size() < this.capacity){
+            this.books.add(book);
+        }
+    }
+}
---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,41 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LibraryTest {
+
+    private Book book;
+    private Library library;
+
+    @Before
+    public void before(){
+        library = new Library(5);
+        book = new Book("Lanark", "Alasdair Gray", "Postmodernism");
+    }
+
+    @Test
+    public void startsWithNoBooks(){
+        assertEquals(0, library.numberOfBooks());
+    }
+
+    @Test
+    public void addBookToLibrary(){
+        library.addBook(book);
+        assertEquals(1, library.numberOfBooks());
+    }
+
+    @Test
+    public void cannotAddBookAsOverCapacity(){
+        library.addBook(book);
+        library.addBook(book);
+        library.addBook(book);
+        library.addBook(book);
+        library.addBook(book);
+        library.addBook(book);
+        assertEquals(5, library.numberOfBooks());
+    }
+
+
+
+}
